c39e8042691f86d769bbfdb1f36c137199693a6c
natanzinho-atp <natanaelj58@gmail.com>
2026-03-24 20:50:07 -0300

    segundo deploy

---
+++ sistemas/Program.vb
@@ -0,0 +1,16 @@
+Imports System
+
+Module Program
+    Sub Main(args As String())
+        Dim sistema(3) As String
+
+        sistema(0) = "Linux"
+        sistema(1) = "Windows"
+        sistema(2) = "Ios"
+
+        console.WriteLine(sistema(0))
+        console.WriteLine(sistema(1))
+        console.WriteLine(sistema(2))
+        
+    End Sub 
+End Module
