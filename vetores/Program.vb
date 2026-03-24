28324d3d1492f5b138e2fbc32f4432f3fce60e5d
natanzinho-atp <natanaelj58@gmail.com>
2026-03-24 20:21:51 -0300

    primeirodeploy

---
+++ vetores/Program.vb
@@ -0,0 +1,12 @@
+Imports System
+
+Module Program
+    Sub Main(args As String())
+       Dim NomeCompleto(2) As String
+
+       NomeCompleto(0) = "Natan"
+       NomeCompleto(1) = "josé"
+       Console.WriteLine(NomeCompleto(0) & NomeCompleto(1))
+
+    End Sub
+End Module
